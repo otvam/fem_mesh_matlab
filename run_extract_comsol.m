3be551d8e122b7b38fa1e77cb2e2051bd450e6dd
Thomas Guillod <guillod@otvam.ch>
2020-02-02 19:32:46 +0100

    Documentation for the COMSOL extraction

--- run_extract_comsol.m
+++ run_extract_comsol.m
@@ -1,21 +1,22 @@
 function run_extract_comsol()
+%RUN_EXTRACT_COMSOL Extract 2d/3d mesh and data from COMSOL simulations.
+%
+%   Two different models are considered:
+%      - 'simple' - a simple 2d and 3d electrostatic model meant as a basic example
+%      - 'bridge' - a complex 3d structural analysis model meant as a show-off
+%
+%   This MATLAB toolbox is indepedent of COMSOL and can work with any triangulated data.
+%   COMSOL is just used for generating the data for the examples.
+%
+%   See also RUN_BRIDGE_EXAMPLE, RUN_SIMPLE_EXAMPLE, MPHLOAD, MPHEVAL.
+
+%   Thomas Guillod.
+%   2020 - BSD License.
 
 close('all')
 addpath('fem_mesh_utils')
 
-%% bridge
-
-% load
-model = load_comsol('model_bridge/bridge.mph');
-
-% data
-data_edge = extract_bridge(model, 'dset1', 'adj2', 'edge_3d');
-data_surface = extract_bridge(model, 'dset1', 'adj1', 'surface_3d');
-
-% save
-save('model_bridge/bridge.mat', 'data_edge', 'data_surface')
-
-%% simple
+%% simple / 2d and 3d electrostatic model
 
 % load
 model = load_comsol('model_simple/simple.mph');
@@ -32,14 +33,77 @@ data_3d.volume = extract_simple_3d(model, 'dset2', 'sel6', 'volume_3d');
 % save
 save('model_simple/simple.mat', 'data_2d', 'data_3d')
 
+%% bridge / 3d structural analysis model
+
+% load
+model = load_comsol('model_bridge/bridge.mph');
+
+% extract data
+data_edge = extract_bridge(model, 'dset1', 'adj2', 'edge_3d');
+data_surface = extract_bridge(model, 'dset1', 'adj1', 'surface_3d');
+
+% save
+save('model_bridge/bridge.mat', 'data_edge', 'data_surface')
+
+end
+
+function data = extract_simple_2d(model, dataset, sel, type)
+%EXTRACT_SIMPLE_2D Extract the data for the 2d electrostatic model.
+%   data = EXTRACT_SIMPLE_2D(model, dataset, sel, type)
+%   model - COMSOL model livelink object (COMSOL model)
+%   dataset - name of the dataset (string)
+%   sel - name of the selection containing the geometry (string)
+%   type - type of the geometry (string)
+%   data - extracted geometry and data (struct)
+
+% extract data from COMSOL
+expr = {'es_2d.Ex', 'es_2d.Ey', 'es_2d.normE'};
+[geom_fem, value_fem] = extract_comsol(model, dataset, sel, type, expr);
+
+% assign data
+data.geom_fem = geom_fem;
+data.E_x = value_fem{1};
+data.E_y = value_fem{2};
+data.E = value_fem{3};
+
+end
+
+function data = extract_simple_3d(model, dataset, sel, type)
+%EXTRACT_SIMPLE_3D Extract the data for the 3d electrostatic model.
+%   data = EXTRACT_SIMPLE_3D(model, dataset, sel, type)
+%   model - COMSOL model livelink object (COMSOL model)
+%   dataset - name of the dataset (string)
+%   sel - name of the selection containing the geometry (string)
+%   type - type of the geometry (string)
+%   data - extracted geometry and data (struct)
+
+% extract data from COMSOL
+expr = {'es_3d.Ex', 'es_3d.Ey', 'es_3d.Ez', 'es_3d.normE'};
+[geom_fem, value_fem] = extract_comsol(model, dataset, sel, type, expr);
+
+% assign data
+data.geom_fem = geom_fem;
+data.E_x = value_fem{1};
+data.E_y = value_fem{2};
+data.E_z = value_fem{3};
+data.E = value_fem{4};
+
 end
 
 function data = extract_bridge(model, dataset, sel, type)
+%EXTRACT_BRIDGE Extract the data for the 3d structural analysis model.
+%   data = EXTRACT_BRIDGE(model, dataset, sel, type)
+%   model - COMSOL model livelink object (COMSOL model)
+%   dataset - name of the dataset (string)
+%   sel - name of the selection containing the geometry (string)
+%   type - type of the geometry (string)
+%   data - extracted geometry and data (struct)
 
-% geom
+% extract data from COMSOL
 expr = {'u', 'v', 'w', 'solid.disp'};
 [geom_fem, value_fem] = extract_comsol(model, dataset, sel, type, expr);
 
+% assign data
 data.geom_fem = geom_fem;
 data.disp_x = value_fem{1};
 data.disp_y = value_fem{2};
@@ -49,34 +113,11 @@ data.disp_mat = [value_fem{1:3}];
 
 end
 
-function data = extract_simple_2d(model, dataset, sel, type)
-
-% geom
-expr = {'es_2d.Ex', 'es_2d.Ey', 'es_2d.normE'};
-[geom_fem, value_fem] = extract_comsol(model, dataset, sel, type, expr);
-
-data.geom_fem = geom_fem;
-data.E_x = value_fem{1};
-data.E_y = value_fem{2};
-data.E = value_fem{3};
-
-end
-
-function data = extract_simple_3d(model, dataset, sel, type)
-
-% geom
-expr = {'es_3d.Ex', 'es_3d.Ey', 'es_3d.Ez', 'es_3d.normE'};
-[geom_fem, value_fem] = extract_comsol(model, dataset, sel, type, expr);
-
-data.geom_fem = geom_fem;
-data.E_x = value_fem{1};
-data.E_y = value_fem{2};
-data.E_z = value_fem{3};
-data.E = value_fem{4};
-
-end
-
 function model = load_comsol(filename)
+%LOAD_COMSOL Check if the COMSOL/MATLAB is running and load a model.
+%   model = LOAD_COMSOL(filename)
+%   filename - filesystem path of the model (string)
+%   model - COMSOL model livelink object (COMSOL model)
 
 try
     model = mphload(filename);
@@ -87,6 +128,15 @@ end
 end
 
 function [geom_fem, value_fem] = extract_comsol(model, dataset, sel, type, expr)
+%EXTRACT_COMSOL Extract the geometry and data from a COMSOL model.
+%   [geom_fem, value_fem] = EXTRACT_COMSOL(model, dataset, sel, type, expr)
+%   model - COMSOL model livelink object (COMSOL model)
+%   dataset - name of the dataset (string)
+%   sel - name of the selection containing the geometry (string)
+%   type - type of the geometry (string)
+%   expr - expression to be evaluated on the geometry (cell of string)
+%   geom_fem - raw mesh data from a FEM solver (struct)
+%   data_fem - data from a FEM solver (cell of matrix of float)
 
 % get the data
 data_tmp = model.mpheval(expr,'Dataset', dataset, 'Complexout','on', 'Selection', sel);
@@ -96,6 +146,7 @@ geom_fem.type = type;
 geom_fem.tri = double(data_tmp.t+1).';
 geom_fem.pts = double(data_tmp.p).';
 
+% check the data
 switch type
     case 'edge_2d'
         assert(size(data_tmp.t, 1)==2, 'invalid size')
